6a56b5fa1b55e0a7ee9cd83570b1fff31582011b
Jude Chuks <judechuks26@gmail.com>
2025-07-14 21:25:27 +0100

    updated login page

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -219,7 +219,7 @@ const LoginForm = () => {
               <button
                 type="submit"
                 disabled={isLoading}
-                className={`w-full flex justify-center mt-8 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:bg-blue-700 dark:hover:bg-blue-800 ${
+                className={`w-full flex justify-center mt-8 py-2.5 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:bg-blue-700 dark:hover:bg-blue-800 ${
                   isLoading ? "opacity-75 cursor-not-allowed" : ""
                 }`}
               >
